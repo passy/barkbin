5c1cc94b6330fb3710d959b37ccc5c4322c7254b
Pascal Hartig <phartig@rdrei.net>
2017-12-21 09:59:31 +0100

    Add db open fn

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -2,14 +2,15 @@ use diesel::prelude::*;
 use diesel::sqlite::SqliteConnection;
 use dotenv::dotenv;
 use std::env;
+use failure::{Error, err_msg};
 
 // TODO: Use error-chain or failure here.
-pub fn establish_connection() -> Result<SqliteConnection, String> {
+pub fn establish_connection() -> Result<SqliteConnection, Error> {
     dotenv().ok();
 
     let database_url = env::var("DATABASE_URL")
-        .map_err(|_| "DATABASE_URL must be set.")?;
+        .map_err(|_| err_msg("DATABASE_URL must be set."))?;
 
     SqliteConnection::establish(&database_url)
-        .map_err(|e| format!("Error connecting to {}: {}", database_url, e))
+        .map_err(|e| format_err!("Failed to open database {}: {}", database_url, e))
 }
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 #![plugin(rocket_codegen)]
 
 extern crate rocket;
+#[macro_use] extern crate failure;
 #[macro_use] extern crate diesel;
 extern crate dotenv;
 
