96a582fbfc091a202138a01ba7f8327a16d5f159
Pascal Hartig <phartig@rdrei.net>
2017-12-23 13:03:32 +0100

    Add endpoint for creating pastes

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -2,6 +2,12 @@
 #![plugin(rocket_codegen)]
 
 extern crate rocket;
+extern crate barkbin_backend;
+
+use barkbin_backend::db::{create_bark, establish_connection};
+use rocket::Data;
+use rocket::response::status;
+use std::io::Read;
 
 #[get("/")]
 fn index() -> &'static str {
@@ -19,6 +25,20 @@ fn index() -> &'static str {
     "
 }
 
+#[post("/", data = "<paste>")]
+fn create(paste: Data) -> Result<status::Created<String>, status::BadRequest<()>> {
+    let filename = "test.txt";
+    let mut body = String::new();
+    paste.open().read_to_string(&mut body).unwrap();
+
+    let conn = establish_connection().expect("Don't do this you idiot!");
+
+    match create_bark(&conn, &filename, &body) {
+        Ok(_) => Ok(status::Created("Cool.".to_owned(), None)),
+        Err(_) => Err(status::BadRequest(None)),
+    }
+}
+
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite().mount("/", routes![index, create]).launch();
 }
